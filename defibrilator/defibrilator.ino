8a15a4d610f863364964e73f0e32f8bde7868a21
123 <git config --global user.name 123a.aladov@codefresh.io>
2020-09-23 16:51:57 +0300

    dfsdf

--- defibrilator/defibrilator.ino
+++ defibrilator/defibrilator.ino
@@ -1,33 +1,34 @@
 #include <EEPROM.h>
 #include <Bounce2.h>
 
-Bounce debButton = Bounce();
-Bounce debMagnet = Bounce();
+Bounce DEB_BUTTON = Bounce();
+Bounce DEB_MAGNET = Bounce();
 #define G3 1568
 #define G4 3136
 
-#define pinButton 8
-#define pinMagnet 7
-#define pinVibro1 9
-#define pinVibro2 10
+#define PIN_BUTTON 8
+#define PIN_MAGNET 7
+#define PIN_VIBRO_1 9
+#define PIN_VIBRO_2 10
 
-#define pinLedRed1 2 
-#define pinLedRed2 3
-#define pinLedRed3 4
-#define pinLedRed4 12
-#define pinLedGreen 5
+#define PIN_LED_RED_1 2
+#define PIN_LED_RED_2 3
+#define PIN_LED_RED_3 4
+#define PIN_LED_RED_4 12
+#define PIN_LED_GREEN 5
 
 #define LED_LENGTH 4
 
-int redLeds[LED_LENGTH] = {pinLedRed1, pinLedRed2, pinLedRed3, pinLedRed4}; 
+int redLeds[LED_LENGTH] = {PIN_LED_RED_1, PIN_LED_RED_2, PIN_LED_RED_3, PIN_LED_RED_4};
 int SECOND  = 1000;
-long MINUTES = SECOND * 60;
-long CHARGE_TIME  = 5* SECOND;
-long CHARGE_INTERVAL   = CHARGE_TIME /LED_LENGTH;
+unsigned long MINUTES = SECOND * 60;
+long DEFAULT_CHARGE_TIME  = 5* SECOND;
+unsigned long CHARGE_TIME;
+unsigned long CHARGE_INTERVAL;
 
 long SAVED_CHARGE_TIME;
 
-#define  pinSound 13
+#define  PIN_SOUND 13
 
 const int SOUND_LENGTH = 200;
 const int SOUND_BASE_FREQ = 1174;
@@ -36,50 +37,49 @@ String state;
 
 bool blocked=false;
 
-
 void setup() {
   Serial.begin(9600);
 
-  pinMode(pinLedRed1, OUTPUT);
-  pinMode(pinLedRed2, OUTPUT);
-  pinMode(pinLedRed3, OUTPUT);
-  pinMode(pinLedRed4, OUTPUT);
-  pinMode(pinLedGreen, OUTPUT);
+  pinMode(PIN_LED_RED_1, OUTPUT);
+  pinMode(PIN_LED_RED_2, OUTPUT);
+  pinMode(PIN_LED_RED_3, OUTPUT);
+  pinMode(PIN_LED_RED_4, OUTPUT);
+  pinMode(PIN_LED_GREEN, OUTPUT);
 
-  pinMode(pinSound, OUTPUT);
+  pinMode(PIN_SOUND, OUTPUT);
 
-  pinMode(pinButton, INPUT_PULLUP);
-  pinMode(pinMagnet, INPUT_PULLUP);
-  pinMode(pinVibro1, OUTPUT);
-  pinMode(pinVibro2, OUTPUT);
-  
-  debButton.attach(pinButton);
-  debButton.interval(5);
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  pinMode(PIN_MAGNET, INPUT_PULLUP);
+  pinMode(PIN_VIBRO_1, OUTPUT);
+  pinMode(PIN_VIBRO_2, OUTPUT);
 
-  debMagnet.attach(pinMagnet);
-  debMagnet.interval(5);
+  DEB_BUTTON.attach(PIN_BUTTON);
+  DEB_BUTTON.interval(5);
 
-  SAVED_CHARGE_TIME = EEPROM.read(0); 
+  DEB_MAGNET.attach(PIN_MAGNET);
+  DEB_MAGNET.interval(5);
 
-  
-  if(SAVED_CHARGE_TIME != 255){
-    CHARGE_TIME  = SAVED_CHARGE_TIME* MINUTES;
-    CHARGE_INTERVAL   = CHARGE_TIME /LED_LENGTH;
-  }
- 
+  SAVED_CHARGE_TIME = EEPROM.read(0);
+
+  _setChargeTime(DEFAULT_CHARGE_TIME*10);
+//   SAVED_CHARGE_TIME == 255
+//   ? _setChargeTime(DEFAULT_CHARGE_TIME)
+//   : _setChargeTime(SAVED_CHARGE_TIME* MINUTES);
+
+
   state="charge";
   Serial.println("Start");
 }
 
 void loop() {
-  debButton .update();
-  debMagnet.update();
-  changeState();  
+  DEB_BUTTON .update();
+  DEB_MAGNET.update();
+  changeState();
 }
 
 void changeState(){
   if (blocked) return;
-  
+
   if(state=="charge"){
     charge();
   }else if (state == "charged"){
@@ -93,62 +93,26 @@ void changeState(){
   }else if (state == "discharge"){
      discharge();
   } else if(state == "postSetup"){
-    postSetup(); 
+    postSetup();
   }
 }
 
 
-int pressedTime=0;  
-void postSetup(){
-//    
-//    if(BUTTON_RESET.changed() && !BUTTON_RESET.read()){
-//      Serial.println("++");
-//        tone(PIN_SOUND, G3, 100);
-//        pressedTime++;
-//    }
-//    if(BUTTON_GREEN.changed() && !BUTTON_GREEN.read()){
-//      Serial.println("pressedTime>>");
-//      Serial.println(pressedTime);
-//      if(!pressedTime)  pressedTime= DEFAULT_RETENTION_MIN;
-//      RETENTION_TIMOUT = MINUTES * pressedTime;
-//      if(pressedTime<=20){
-//        for(int i=0; i<pressedTime; i++){
-//              delay(200);
-//              tone(PIN_SOUND, G3, 200);
-//              digitalWrite(PIN_LED_GREEN, HIGH);
-//              delay(200);
-//              digitalWrite(PIN_LED_GREEN, LOW);
-//            }
-//      }else{
-//        tone(PIN_SOUND, G3, 800);
-//        digitalWrite(PIN_LED_GREEN, HIGH);
-//        delay(400);
-//        digitalWrite(PIN_LED_GREEN, LOW);
-//        
-//        digitalWrite(PIN_LED_RED, HIGH);
-//        delay(400);
-//        digitalWrite(PIN_LED_RED, LOW);
-//        
-//        digitalWrite(PIN_LED_GREEN, HIGH);
-//        delay(400);
-//        digitalWrite(PIN_LED_GREEN, LOW);
-//      }
-//      
-//      
-      state="charge";
-//    }
-}
-
 unsigned long chargeTimestamp=0;
 int ledToActivateIndex = 0;
+bool tunedIn =false;
 void charge(){
    unsigned long currentTime=millis();
-   
-   if ((currentTime - chargeTimestamp) > CHARGE_INTERVAL){      
+   if (_longSignal(LOW, 2*SECOND, DEB_BUTTON) && !tunedIn){
+       tunedIn = true;
+       _setupEffects();
+       state="postSetup";
+   }
+   else if ((currentTime - chargeTimestamp) > CHARGE_INTERVAL){
       chargeTimestamp = currentTime;
       digitalWrite(redLeds[ledToActivateIndex++], HIGH);
-      tone(pinSound, SOUND_BASE_FREQ, SOUND_LENGTH); 
-      
+      tone(PIN_SOUND, SOUND_BASE_FREQ, SOUND_LENGTH);
+
       if (!redLeds[ledToActivateIndex]){
         ledToActivateIndex=0;
         chargeTimestamp=0;
@@ -159,11 +123,34 @@ void charge(){
    }
 }
 
+int pressedTime=0;
+void postSetup(){
+    if (_longSignal(LOW, 2*SECOND, DEB_BUTTON)){
+        Serial.println("pressedTime::");
+        Serial.println(pressedTime);
+        if(pressedTime){
+            _setChargeTime(pressedTime* MINUTES);
+            _saveChargeTimeToMemory(pressedTime);
+        }else{
+            _setChargeTime(DEFAULT_CHARGE_TIME);
+        }
+        _setupEffects();
+        state="charge";
+    }
+    else if(DEB_BUTTON.changed() && !DEB_BUTTON.read()){
+        Serial.println("++");
+        tone(PIN_SOUND, G3, 100);
+        pressedTime++;
+    }
+}
+
+
+
 int touchCount=0;
 void charged() {
-  // need to activate magnet 
+  // need to activate magnet
   const int TOUCH_LIMIT=6;
-  if(debMagnet.changed() && !debMagnet.read()){
+  if(DEB_MAGNET.changed() && !DEB_MAGNET.read()){
     ++touchCount;
   }
   if(touchCount>TOUCH_LIMIT){
@@ -175,7 +162,7 @@ void charged() {
 }
 
 void waitForReleaseMagnet(){
-  if(_longSignal(HIGH, SECOND*0.4, debMagnet)){
+  if(_longSignal(HIGH, SECOND*0.4, DEB_MAGNET)){
     Serial.println("waitForReleaseMagnet -> waitForPressButton");
     state = "waitForPressButton";
   }
@@ -185,7 +172,7 @@ void waitForReleaseMagnet(){
 unsigned long timeoutStartTime = 0;
 void waitForPressButton(){
   long TIMEOUT = 5*SECOND;
-  if(debButton.changed() && !debButton.read()){
+  if(DEB_BUTTON.changed() && !DEB_BUTTON.read()){
     Serial.println("waitForPressButton-> waitForReleaseButton");
     timeoutStartTime=0;
     state = "waitForReleaseButton";
@@ -202,7 +189,7 @@ void waitForPressButton(){
 }
 
 void waitForReleaseButton(){
-  if(debButton.changed() && debButton.read()){
+  if(DEB_BUTTON.changed() && DEB_BUTTON.read()){
     Serial.println("waitForReleaseButton -> discharge");
     state = "discharge";
   }
@@ -212,24 +199,24 @@ void waitForReleaseButton(){
 void discharge(){
   Serial.println("Discharge");
   const int DELAY_LENGTH = 1000;
-  
-  digitalWrite(pinLedGreen, LOW);
-  digitalWrite(pinVibro1, HIGH);
-  digitalWrite(pinVibro2, HIGH);
-  
-  tone(pinSound,G4);
-  delay(800);
-  noTone(pinSound);
 
-  digitalWrite(pinVibro1, LOW);
-  digitalWrite(pinVibro2, LOW);
-  
+  digitalWrite(PIN_LED_GREEN, LOW);
+  digitalWrite(PIN_VIBRO_1, HIGH);
+  digitalWrite(PIN_VIBRO_2, HIGH);
+
+  tone(PIN_SOUND,G4);
+  delay(800);
+  noTone(PIN_SOUND);
+
+  digitalWrite(PIN_VIBRO_1, LOW);
+  digitalWrite(PIN_VIBRO_2, LOW);
+
   for(int i=G4; i>=0; i-=20){
-      tone(pinSound,i);
+      tone(PIN_SOUND,i);
       delay(4);
-      noTone(pinSound);
+      noTone(PIN_SOUND);
   }
-  
+
   delay(2000);
 
   state = "charge";
@@ -237,14 +224,30 @@ void discharge(){
 }
 
 
+void _setChargeTime(unsigned long chargeTime){
+    CHARGE_TIME  = chargeTime;
+    CHARGE_INTERVAL   = CHARGE_TIME /LED_LENGTH;
+    Serial.println("chargeTime::");
+    Serial.println(chargeTime);
+    Serial.println("CHARGE_INTERVAL::");
+    Serial.println(CHARGE_INTERVAL);
+
+}
+
+void _saveChargeTimeToMemory(int timeInMinutes){
+    SAVED_CHARGE_TIME == 255
+              ? EEPROM.write(0, timeInMinutes)
+              : EEPROM.update(0, timeInMinutes);
+}
+
 void _magnetIsDisactivated() {
-  tone(pinSound, G3, SECOND);
+  tone(PIN_SOUND, G3, SECOND);
 
   for(int i=0; i<3; i++){
     _clearAllLeds();
     delay(200);
     _allLedsRed();
-    if (i<2) delay(200); 
+    if (i<2) delay(200);
   }
 }
 
@@ -252,36 +255,53 @@ void _magnetIsDisactivated() {
 void _magnetIsActivated() {
 
   for(int i=0; i<3; i++){
-    tone(pinSound, G3, 100);
+    tone(PIN_SOUND, G3, 100);
     _clearAllLeds();
     delay(100);
     _allLedsGreen();
-    if (i<2) delay(100); 
+    if (i<2) delay(100);
   }
 }
 
 void _allLedsGreen(){
-  digitalWrite(pinLedRed1, LOW);
-  digitalWrite(pinLedRed2, LOW);
-  digitalWrite(pinLedRed3, LOW);
-  digitalWrite(pinLedRed4, LOW);
-  digitalWrite(pinLedGreen, HIGH);
+  digitalWrite(PIN_LED_RED_1, LOW);
+  digitalWrite(PIN_LED_RED_2, LOW);
+  digitalWrite(PIN_LED_RED_3, LOW);
+  digitalWrite(PIN_LED_RED_4, LOW);
+  digitalWrite(PIN_LED_GREEN, HIGH);
 }
 
 void _allLedsRed(){
-  digitalWrite(pinLedGreen, LOW);
-  digitalWrite(pinLedRed1, HIGH);
-  digitalWrite(pinLedRed2, HIGH);
-  digitalWrite(pinLedRed3, HIGH);
-  digitalWrite(pinLedRed4, HIGH);
+  digitalWrite(PIN_LED_GREEN, LOW);
+  digitalWrite(PIN_LED_RED_1, HIGH);
+  digitalWrite(PIN_LED_RED_2, HIGH);
+  digitalWrite(PIN_LED_RED_3, HIGH);
+  digitalWrite(PIN_LED_RED_4, HIGH);
 }
 
 void _clearAllLeds(){
-  digitalWrite(pinLedGreen, LOW);
-  digitalWrite(pinLedRed1, LOW);
-  digitalWrite(pinLedRed2, LOW);
-  digitalWrite(pinLedRed3, LOW);
-  digitalWrite(pinLedRed4, LOW);
+  digitalWrite(PIN_LED_GREEN, LOW);
+  digitalWrite(PIN_LED_RED_1, LOW);
+  digitalWrite(PIN_LED_RED_2, LOW);
+  digitalWrite(PIN_LED_RED_3, LOW);
+  digitalWrite(PIN_LED_RED_4, LOW);
+}
+
+void _setupEffects(){
+        tone(PIN_SOUND, G3, 200);
+       _allLedsGreen();
+       delay(400);
+
+
+       tone(PIN_SOUND, G3, 200);
+       _allLedsRed();
+       delay(400);
+
+       tone(PIN_SOUND, G3, 200);
+       _allLedsGreen();
+       delay(400);
+
+       _clearAllLeds();
 }
 
 unsigned long startTime=0;
@@ -291,13 +311,11 @@ bool _longSignal(int expectedValue, long timeout, Bounce debouncer){
     unsigned long currentTime=millis();
     if(!startTime) startTime = currentTime;
     if((currentTime - startTime)>timeout){
-      Serial.println("_longSignal!");
-      Serial.println(currentTime - startTime);
       startTime=0;
       return true;
     }
   }else{
-   startTime=0; 
-  }  
+   startTime=0;
+  }
   return false;
 }
